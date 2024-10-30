3100708f80d7ed82b6bfdaecb4fd9a35c47b78e0
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2024-10-30 18:45:40 +0300

    aside styles save

--- src/Components/AsideFilter/FilterList/FilterRow/FilterRow.jsx
+++ src/Components/AsideFilter/FilterList/FilterRow/FilterRow.jsx
@@ -1,11 +1,21 @@
 import './FilterRow.less'
+import {useState} from "react";
 
 const FilterRow = ({name, id}) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleChecked = () => {setChecked(prev => !prev);};
 
     return (
-        <li className="filter-row" >
-            <input className="filter-checkbox" id={id} type="checkbox"/>
-            <label htmlFor={id}>{name}</label>
+        <li className={`filter-row ${checked ? 'checked-bg' : '' }`} >
+            <label className="filter-label" htmlFor={id}>
+                <input className="filter-checkbox"
+                       id={id} type="checkbox"
+                       checked={checked}
+                       onChange={handleChecked}
+                />
+                {name}
+            </label>
         </li>
     )
 }
