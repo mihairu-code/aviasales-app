d8265d549fbaca04d5d0f30abba73e403d29da9a
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2024-11-01 01:36:31 +0300

    server part done

--- src/Components/Aviasales/Aviasales.jsx
+++ src/Components/Aviasales/Aviasales.jsx
@@ -1,9 +1,14 @@
+import TicketsArray from "../../configuration.js";
 import './Aviasales.less'
 import Logo from "../Logo/Logo.jsx";
 import MainAviasales from "../MainAviasales/MainAviasales.jsx";
 import AsideFilter from "../AsideFilter/AsideFilter.jsx";
 
+
 const Aviasales = () => {
+
+
+
     return (
         <div className="aviasales">
             <Logo />
