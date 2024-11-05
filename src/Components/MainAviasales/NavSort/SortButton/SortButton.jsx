e02f03708986bf71cc5d7ea8ff3be425b68d433a
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2024-11-05 22:42:28 +0300

    top filter tottaly done

--- src/Components/MainAviasales/NavSort/SortButton/SortButton.jsx
+++ src/Components/MainAviasales/NavSort/SortButton/SortButton.jsx
@@ -1,9 +1,16 @@
 import './SortButton.less'
+import { setSort } from "../../../../store/aviaSlices.js";
+import {useDispatch, useSelector } from "react-redux";
 
 const SortButton = ({name}) => {
+    const dispatch = useDispatch();
+    const currentSort = useSelector(state => state.avia.sort);
+    const handleSortClick = () => {
+        dispatch(setSort(name));
+    };
 
     return (
-        <button className="sort-button">{name}</button>
+        <button className={`sort-button ${currentSort === name ? "active" : ""}`} onClick={handleSortClick}>{name}</button>
     )
 }
 
--- src/Components/MainAviasales/TicketsList/TicketsList.jsx
+++ src/Components/MainAviasales/TicketsList/TicketsList.jsx
@@ -7,18 +7,39 @@ const TicketsList = () => {
 
     const tickets = useSelector(state => state.avia.tickets);
     const visibledTickets = useSelector(state => state.avia.visibledTickets)
+    const sort = useSelector(state => state.avia.sort);
+    const filters = useSelector(state => state.avia.filters);
     const dispatch = useDispatch();
 
     const showMoreTickets = () => {
         dispatch(setVisibledTickets(visibledTickets + 5));
     };
 
+    const filteredTickets = tickets.filter(ticket => {
+        if (filters.length === 0) return true; // Если нет фильтров, показываем все билеты
+        return filters.includes(ticket.segments[0].stops.length); // Фильтруем по количеству пересадок
+    });
+
+    const sortedTickets = filteredTickets.sort((a, b) => {
+        if (sort === 'Самый быстрый') {
+            return a.segments[0].duration - b.segments[0].duration; // Сортировка по времени
+        }
+        if (sort === 'Самый дешевый') {
+            return a.price - b.price; // Сортировка по цене
+        }
+        return 0; // Без изменений
+    });
+
     return (
         <section className="tickets-section">
             <ul className="tickets-list">
-                {tickets.slice(0, visibledTickets).map((ticket, index) => (<Ticket key={index} ticket={ticket}/>))}
+                {sortedTickets.slice(0, visibledTickets).map((ticket, index) => (<Ticket key={index} ticket={ticket}/>))}
             </ul>
-            <button className="show-more-button" onClick={showMoreTickets}>Показать еще</button>
+            {visibledTickets < sortedTickets.length && (
+                <button className="show-more-button" onClick={showMoreTickets}>
+                    Показать еще
+                </button>
+            )}
         </section>
     )
 }
